a14721abcbc93e3831df357a8edbdfda37d294c5
Manuel Heredia <Spectrasonic117@gmail.com>
2023-08-15 11:40:16 -0600

    Fixed pinchi coso

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,9 +19,9 @@ local default_plugins = {
     "NvChad/ui",
     branch = "v2.0",
     lazy = false,
- --   config = function()
- --     require "nvchad_ui"
- --   end,
+    -- config = function()
+    --   require "nvchad"
+    -- end,
   },
 
   {
