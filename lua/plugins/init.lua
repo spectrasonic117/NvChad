03aac14db286b4c9756956a9d46a246a8abb71f0
Manuel Heredia <spectra@MacBook-Pro-de-Manuel.local>
2024-09-18 10:56:56 -0600

    add SuperMaven as AI

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -262,19 +262,19 @@ local default_plugins = {
             require("which-key").setup(opts)
         end,
     },
-    {
-        'Exafunction/codeium.vim',
-        event = 'BufEnter',
-        config = function()
-            -- Change '<C-g>' here to any keycode you like.
-            vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-                { expr = true, silent = true })
-            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-                { expr = true, silent = true })
-            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-        end
-    },
+    -- {
+    --     'Exafunction/codeium.vim',
+    --     event = 'BufEnter',
+    --     config = function()
+    --         -- Change '<C-g>' here to any keycode you like.
+    --         vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    --         vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+    --             { expr = true, silent = true })
+    --         vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+    --             { expr = true, silent = true })
+    --         vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    --     end
+    -- },
     {
         'stevearc/conform.nvim',
         opts = {},
@@ -284,6 +284,26 @@ local default_plugins = {
         config = function()
             vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
         end,
+    },
+    {
+      	"supermaven-inc/supermaven-nvim",
+		lazy=false,
+      	config = function()
+        	require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<Tab>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-j>",
+  				},
+				ignore_filetypes = { cpp = false }, -- or { "cpp", }
+				log_level = "info", -- set to "off" to disable logging completely
+				disable_inline_completion = false, -- disables inline completion for use with cmp
+				disable_keymaps = false, -- disables built in keymaps for more manual control
+				condition = function()
+					return false
+				end
+			})
+      end,
     },
 }
 
